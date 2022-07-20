5f62f846d7470d906cb50d94f43445c12654ab41
Oleksii Kostetskyi <okostet@softserveinc.com>
2022-07-20 15:36:27 +0300

    Use TypeId Mappings to prevent Events classes movement

--- src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
+++ src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
@@ -1,11 +1,15 @@
 package guru.springframework.msscbeerservice.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.sfg.beer.common.events.NewInventoryEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by jt on 2019-07-20.
@@ -15,6 +19,10 @@ public class JmsConfig {
 
     public static final String BREWING_REQUEST_QUEUE = "brewing-request";
     public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+    private static final Map<String, Class<?>> TYPE_ID_MAPPINGS =  new HashMap<>();
+    static {
+        TYPE_ID_MAPPINGS.put("NewInventoryEvent", NewInventoryEvent.class);
+    }
 
     @Bean // Serialize message content to json using TextMessage
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
@@ -22,6 +30,7 @@ public class JmsConfig {
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
         converter.setObjectMapper(objectMapper);
+        converter.setTypeIdMappings(TYPE_ID_MAPPINGS);
         return converter;
     }
 }
